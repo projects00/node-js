97313b779c6ea1987f7c3fda3b3b19baa933160d
projects00 <34448034+projects00@users.noreply.github.com>
2018-03-18 21:37:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.use(passport.session());
 
 // cross origin mioddleware
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://sportsapp.netlify.com");
     res.header("Access-Control-Allow-Credentials", 'true');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
